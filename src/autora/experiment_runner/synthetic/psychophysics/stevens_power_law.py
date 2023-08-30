daf522fd25a9806ccacb09b0d0a7beec321477e5
TheLemonPig <43144407+TheLemonPig@users.noreply.github.com>
2023-08-30 14:52:33 -0400

    Update src/autora/experiment_runner/synthetic/psychophysics/stevens_power_law.py

--- src/autora/experiment_runner/synthetic/psychophysics/stevens_power_law.py
+++ src/autora/experiment_runner/synthetic/psychophysics/stevens_power_law.py
@@ -2,6 +2,7 @@ from functools import partial
 from typing import Optional, Union
 
 import numpy as np
+import pandas as pd
 
 from autora.experiment_runner.synthetic.utilities import SyntheticExperimentCollection
 from autora.variable import DV, IV, ValueType, VariableCollection
